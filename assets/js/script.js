31426146abeb2a3192c64d06838c5ace877a88f6
Nick9850 <Nicholas.reitmeyer@gmail.com>
2022-07-26 16:26:11 -0700

    more api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,29 @@
 // Currents API
 function fetchData() {
-var url = 'https://api.currentsapi.services/v1/latest-news?' +
-'language=us&' +
-'apiKey=FMFdCCVwVZ-6TYYMhaG4j_62G-wT-ftp_Dx00FgCgtekS6Wz';
-var req = new Request(url);
-fetch(req)
-.then(response => {
-    if (!response.ok){
-        throw Error("ERROR");
-    }
-    return response.json();
-})
-.then(data => {
-    console.log(data.data);
-    const html = data.data
-        .map(user => {
+    var url = 'https://api.currentsapi.services/v1/latest-news?' +
+    'language=us&' +
+    'apiKey=FMFdCCVwVZ-6TYYMhaG4j_62G-wT-ftp_Dx00FgCgtekS6Wz';
+    var req = new Request(url);
+    fetch(req)
+    .then(response => {
+        if (!response.ok){
+            throw Error("ERROR");
+        }
+        return response.json();
+    })
+    .then(data => {
+        const html = data.data
+        .map( user => {
             return `
             <div class="user">
                 <p><img src="${image}"/></p>
                 <p>Title: ${title}</p>
                 <p>Description: ${description}</p>
-
             </div>
             `;
         })
         .join("");
-        console.log(html);
-        document.querySelector("#app").insertAdjacentHTML("afterbegin", html);        
+    document.querySelector("#app").insertAdjacentHTML("afterbegin", html);        
 })
 .catch (error => {
     console.log(error);
@@ -34,6 +31,35 @@ fetch(req)
 }
 fetchData();
 
+function postData() {
+    var url = 'https://api.currentsapi.services/v1/latest-news?' +
+    'language=us&' +
+    'apiKey=FMFdCCVwVZ-6TYYMhaG4j_62G-wT-ftp_Dx00FgCgtekS6Wz';
+    var req = new Request(url);
+    fetch(req, {
+        method: "POST",
+        headers: {
+            "Content-Type:": "application/json"
+        },
+        body: {
+            name: "morb",
+            job: "in time"
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw Error("ERROR");
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+}
+postData();
 // News API
 var requestOptions = {
     method: 'GET'
@@ -48,20 +74,20 @@ var params = {
 
 
 // Moment JS- for the current day in the trending session
-var trending = document.querySelector(".currentday");
-var currentDay = moment().format("MMM Do YY");
-console.log(currentDay);
-trending.innerHTML = currentDay
+//var trending = document.querySelector(".currentday");
+//var currentDay = moment().format("MMM Do YY");
+//console.log(currentDay);
+//trending.innerHTML = currentDay
 //console.log(currentDay)
 
 
-var esc = encodeURIComponent;
-var query = Object.keys(params)
-    .map(function(k) {return esc(k) + '=' + esc(params[k]);})
-    .join('&');
+//var esc = encodeURIComponent;
+//var query = Object.keys(params)
+//    .map(function(k) {return esc(k) + '=' + esc(params[k]);})
+//    .join('&');
 
-fetch("https://api.thenewsapi.com/v1/news/all?" + query, requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+//fetch("https://api.thenewsapi.com/v1/news/all?" + query, requestOptions)
+//  .then(response => response.text())
+//  .then(result => console.log(result))
+//  .catch(error => console.log('error', error));
 
