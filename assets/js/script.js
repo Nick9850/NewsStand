f0007e41f3dde47122ca91938631407787219da5
Brenda  Sosa <brenda.sosa@gmail.com>
2022-07-26 15:28:07 -0400

    js merge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 var url = 'https://api.currentsapi.services/v1/latest-news?' +
 'language=us&' +
 'apiKey=FMFdCCVwVZ-6TYYMhaG4j_62G-wT-ftp_Dx00FgCgtekS6Wz';
@@ -7,7 +7,7 @@ fetch(req)
 .then(function(response) {
     console.log(response.json());
 })
-=======
+
 
 
 
@@ -17,4 +17,4 @@ var currentDay = moment().format("MMM Do YY");
 console.log(currentDay);
 trending.innerHTML = currentDay
 //console.log(currentDay)
->>>>>>> 2a309411a382110173676d8ab40abd7f892abf3d
+
