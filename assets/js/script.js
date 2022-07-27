1af5cf6488a5221351774a5cfc55dbdfc0d7e497
Nick9850 <Nicholas.reitmeyer@gmail.com>
2022-07-26 17:31:28 -0700

    working api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,85 +1,85 @@
 // Currents API
 function fetchData() {
-    var url = 'https://api.currentsapi.services/v1/latest-news?' +
-    'language=us&' +
-    'apiKey=FMFdCCVwVZ-6TYYMhaG4j_62G-wT-ftp_Dx00FgCgtekS6Wz';
-    var req = new Request(url);
-    fetch(req)
-    .then(response => {
-        if (!response.ok){
-            throw Error("ERROR");
-        }
-        return response.json();
+  console.log("inside fetch data");
+  var url =
+    "https://api.currentsapi.services/v1/latest-news?apiKey=FMFdCCVwVZ-6TYYMhaG4j_62G-wT-ftp_Dx00FgCgtekS6Wz";
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
     })
-    .then(data => {
-        const html = data.data
-        .map( user => {
-            return `
-            <div class="user">
-                <p><img src="${image}"/></p>
-                <p>Title: ${title}</p>
-                <p>Description: ${description}</p>
-            </div>
-            `;
+    .then((data) => {
+      console.log(data);
+      const html = data.news
+        .map((artical) => {
+          return `
+              <div class="trending">
+                  <p><img src="${artical.image}"/></p>
+                  <p>Title: ${artical.title}</p>
+                  <p>Description: ${artical.description}</p>
+              </div>
+              `;
         })
         .join("");
-    document.querySelector("#app").insertAdjacentHTML("afterbegin", html);        
-})
-.catch (error => {
-    console.log(error);
-});
+      document.querySelector("#app").insertAdjacentHTML("afterbegin", html);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  console.log("hello");
 }
-fetchData();
 
 function postData() {
-    var url = 'https://api.currentsapi.services/v1/latest-news?' +
-    'language=us&' +
-    'apiKey=FMFdCCVwVZ-6TYYMhaG4j_62G-wT-ftp_Dx00FgCgtekS6Wz';
-    var req = new Request(url);
-    fetch(req, {
-        method: "POST",
-        headers: {
-            "Content-Type:": "application/json"
-        },
-        body: {
-            name: "morb",
-            job: "in time"
-        }
+  var url =
+    "https://api.currentsapi.services/v1/latest-news?" +
+    "language=us&" +
+    "apiKey=FMFdCCVwVZ-6TYYMhaG4j_62G-wT-ftp_Dx00FgCgtekS6Wz";
+  var req = new Request(url);
+  fetch(req, {
+    method: "POST",
+    headers: {
+      "Content-Type:": "application/json",
+    },
+    body: {
+      name: "",
+      job: "",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw Error("ERROR");
+      }
+      return response.json();
     })
-    .then(response => {
-        if (!response.ok) {
-            throw Error("ERROR");
-        }
-        return response.json();
+    .then((data) => {
+      console.log(data);
     })
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.log(error);
-    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
-postData();
 // News API
 var requestOptions = {
-    method: 'GET'
+  method: "GET",
 };
 
 var params = {
-    api_token: 'hCFrFJecxlYxm8RgxEGwDdaJlZFDNkgw5ZU6vf6j',
-    categories: 'business,tech',
-    search: 'apple',
-    limit: '50'
+  api_token: "hCFrFJecxlYxm8RgxEGwDdaJlZFDNkgw5ZU6vf6j",
+  categories: "business,tech",
+  search: "apple",
+  limit: "50",
 };
 
-
+fetchData();
+//postData();
 // Moment JS- for the current day in the trending session
 //var trending = document.querySelector(".currentday");
 //var currentDay = moment().format("MMM Do YY");
 //console.log(currentDay);
 //trending.innerHTML = currentDay
 //console.log(currentDay)
-
 
 //var esc = encodeURIComponent;
 //var query = Object.keys(params)
@@ -90,4 +90,3 @@ var params = {
 //  .then(response => response.text())
 //  .then(result => console.log(result))
 //  .catch(error => console.log('error', error));
-
